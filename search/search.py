f362f12537aaa438fc255ae5f69d8b4b70390695
MichaelENewbold <michaeleric.newbold@calbaptist.edu>
2025-04-05 16:13:44 -0700

    Search.py being updated to the latest from Project 1

--- search/search.py
+++ search/search.py
@@ -15,6 +15,8 @@
 """
 In search.py, you will implement generic search algorithms which are called by
 Pacman agents (in searchAgents.py).
+
+Code Written by: Michael E Newbold #747638
 """
 
 import util
@@ -92,17 +94,109 @@ def depthFirstSearch(problem):
         print("\t" + str(statePortion))
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    #Initialization
+    from game import Directions
+
+    fringe = util.Stack() # Stack Initialization
+    # In order for this to be a proper stack you can only interact with it as a .append() or .pop()
+    # {If we are using straight Py list}
+
+    fringe.push((problem.getStartState(), []))  # Adds the start state and a list which will be the path
+
+    # Needs to be filled with the directions maybe? Instead of the nodes themselves.
+    explored = [] # Explored nodes, starts as empty, will end up with all explored nodes.
+
+    while not fringe.isEmpty():
+        node, path = fringe.pop() # Gets the next node to look at
+        # path is an updated way to get to the given node, which is always what should be returned
+
+        if problem.isGoalState(node): #
+            return path # Path is what the program is wanting, as it wants the route to the correct node.
+
+        if node not in explored: # DFS does not want to revisit states.
+            explored.append(node) # Adds the node to the explored list
+
+            # Expand the nodes successors, and push them to the stack.
+            # Might have an issue with successor needing to be renamed to nextState
+            for successor, actionFromCurrStateToNextState, _ in problem.getSuccessors(node):
+                if successor not in explored: # DFS does not want to revisit states.
+                    new_path = path + [actionFromCurrStateToNextState] # Updates the path to the new successor
+                    fringe.push((successor, new_path)) # Sends the path with the successor into the fringe.
+    # Fringe is empty, meaning they have explored every node and not found the correct one.
+    return [] # Added list to return correct datatype.
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    # Initialization
+    from game import Directions
+
+    fringe = util.Queue()  # Stack Initialization
+    # In order for this to be a proper stack you can only interact with it as a .append() or .pop()
+    # {If we are using straight Py list}
+
+    fringe.push((problem.getStartState(), []))  # Adds the start state and a list which will be the path
+
+    # Needs to be filled with the directions maybe? Instead of the nodes themselves.
+    explored = []  # Explored nodes, starts as empty, will end up with all explored nodes.
+
+    while not fringe.isEmpty():
+        node, path = fringe.pop()  # Gets the next node to look at
+        # path is an updated way to get to the given node, which is always what should be returned
+
+        if problem.isGoalState(node):  #
+            return path  # Path is what the program is wanting, as it wants the route to the correct node.
+
+        if node not in explored:  # DFS does not want to revisit states.
+            explored.append(node)  # Adds the node to the explored list
+
+            # Expand the nodes successors, and push them to the stack.
+            # Might have an issue with successor needing to be renamed to nextState
+            for successor, actionFromCurrStateToNextState, _ in problem.getSuccessors(node):
+                if successor not in explored:  # DFS does not want to revisit states.
+                    new_path = path + [actionFromCurrStateToNextState]  # Updates the path to the new successor
+                    fringe.push((successor, new_path))  # Sends the path with the successor into the fringe.
+    # Fringe is empty, meaning they have explored every node and not found the correct one.
+    return []  # Added list to return correct datatype.
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    # Initialization
+    from game import Directions
+
+    fringe = util.PriorityQueue()  # Stack Initialization
+    # In order for this to be a proper stack you can only interact with it as a .append() or .pop()
+    # {If we are using straight Py list}
+
+    fringe.push((problem.getStartState(), [], 0), 0)  # Adds the start state and a list which will be the path
+    # Added a third position to hold the Cost of the first item, and a second item to hold the priority level
+
+    # Needs to be filled with the directions maybe? Instead of the nodes themselves.
+    explored = []  # Explored nodes, starts as empty, will end up with all explored nodes.
+
+    while not fringe.isEmpty():
+        node, path, cost = fringe.pop()  # Gets the next node to look at
+        # path is an updated way to get to the given node, which is always what should be returned
+
+        if problem.isGoalState(node):  #
+            return path  # Path is what the program is wanting, as it wants the route to the correct node.
+
+        if node not in explored:  # DFS does not want to revisit states.
+            explored.append(node)  # Adds the node to the explored list
+
+            # Expand the nodes successors, and push them to the stack.
+            # Might have an issue with successor needing to be renamed to nextState
+            for successor, actionFromCurrStateToNextState, stepCost in problem.getSuccessors(node):
+                if successor not in explored:  # DFS does not want to revisit states.
+                    new_path = path + [actionFromCurrStateToNextState]  # Updates the path to the new successor
+                    new_cost = cost + stepCost
+                    fringe.push((successor, new_path, new_cost), new_cost)  # Sends the path with the successor into the fringe.
+    # Fringe is empty, meaning they have explored every node and not found the correct one.
+    return []  # Added list to return correct datatype.
 
 def nullHeuristic(state, problem=None):
     """
@@ -114,7 +208,45 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    # Initialization
+    from game import Directions
+
+    fringe = util.PriorityQueue()  # Stack Initialization
+    # In order for this to be a proper stack you can only interact with it as a .append() or .pop()
+    # {If we are using straight Py list}
+
+    startState = problem.getStartState() # Need to store start state separately now, since we need to use it twice
+
+    fringe.push((startState, [], 0), 0)  # Adds the start state and a list which will be the path
+    # Added a third position to hold the Cost of the first item, and a second item to hold the priority level
+
+    heuristic(startState, problem) # f(n) = g(n) + h(n)
+    # Holds the data that will be used by the heursitic calculations
+
+    # Needs to be filled with the directions maybe? Instead of the nodes themselves.
+    explored = []  # Explored nodes, starts as empty, will end up with all explored nodes.
+
+    while not fringe.isEmpty():
+        node, path, cost = fringe.pop()  # Gets the next node to look at
+        # path is an updated way to get to the given node, which is always what should be returned
+
+        if problem.isGoalState(node):  #
+            return path  # Path is what the program is wanting, as it wants the route to the correct node.
+
+        if node not in explored:  # DFS does not want to revisit states.
+            explored.append(node)  # Adds the node to the explored list
+
+            # Expand the nodes successors, and push them to the stack.
+            # Might have an issue with successor needing to be renamed to nextState
+            for successor, actionFromCurrStateToNextState, stepCost in problem.getSuccessors(node):
+                if successor not in explored:  # DFS does not want to revisit states.
+                    new_path = path + [actionFromCurrStateToNextState]  # Updates the path to the new successor
+                    new_cost = cost + stepCost # Updates the cost to kick to next nodes
+                    priority = new_cost + heuristic(successor, problem) # Gets the priority value that each item should get (f(n) = g(n) + h(n))
+                    fringe.push((successor, new_path, new_cost), priority)  # Sends the path with the successor into the fringe.
+    # Fringe is empty, meaning they have explored every node and not found the correct one.
+    return []  # Added list to return correct datatype.
 
 
 # Abbreviations
